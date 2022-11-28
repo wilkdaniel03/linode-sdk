5544237a41438cb07e5d9647e73bcff5360f8a59
Daniel Wilk <wilkdaniel03@gmail.com>
2022-11-28 10:56:06 +0100

    [auth] Create constructor for token structure

--- src/auth.rs
+++ src/auth.rs
@@ -1,1 +1,14 @@
-pub struct Token<'a> (&'a str);
+use std::env;
+
+pub struct Token (String);
+
+impl Token {
+    pub fn new() -> Self {
+        let token = match env::var("LINODE_TOKEN") {
+            Ok(t) => t,
+            Err(_) => panic!("Failed to find environment variable LINODE_TOKEN")
+        };
+
+        Self(token)
+    }
+}
